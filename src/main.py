ac10bf603bd0894fd0ee318c016f32b381167639
sskhatri <sskhatri@uci.edu>
2025-01-28 10:27:06 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 # Define the path for version.md
 # This assumes the script is run from the root of the repository
-file_path = os.path.join('version-control/output', '/', 'version.md')
+file_path = os.path.join('/output', '/', 'version.md')
 
 # Write the current time to version.md
 with open(file_path, 'w') as f:
