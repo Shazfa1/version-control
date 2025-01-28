6ae86316927d39966015e0c72b4827fca057ec10
sskhatri <ssk@class-docker>
2025-01-28 18:12:18 +0000

    update main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 # Define the path for version.md
 # This assumes the script is run from the root of the repository
-file_path = os.path.join('version-control', '..', 'version.md')
+file_path = os.path.join('version-control/output', '/', 'version.md')
 
 # Write the current time to version.md
 with open(file_pathh, 'w') as f:
